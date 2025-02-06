395d38f7f8ed4d5bbcd259f52b881adac0f3dadc
mohamadaminkha <mohamad.amin.kha.uni@gmail.com>
2025-02-06 11:06:43 +0330

    first comming

---
+++ session 1/main.py
@@ -0,0 +1,45 @@
+#---------------------------
+# int   -1  0  +1
+# str  "salam khobi" 'salam khobi'
+# float    -11.1  0  +11.1
+# bool   True->(+-infinit)   False -> 0
+#----------------------------
+# type()
+# int() float() str() bool()
+#----------------------------
+
+# x = 10
+
+# x = x + 1
+# x += 1
+
+# x = x * 10
+# x *= 10
+
+# x = x % 3 -- mod
+#----------------------------
+# first_name  = "amin"
+# last_name   = "khademi"
+
+# print("welcome " + first_name + " " + last_name)
+# print(f"welcome {first_name} {last_name}")
+
+#----------------------------
+x = "mmd amin"
+
+# print(x.startswith("mmd"))
+# print(x.split(" "))
+# print(x.count("m"))
+# print(x.find("n"))
+
+
+
+
+
+
+
+
+
+
+
+
